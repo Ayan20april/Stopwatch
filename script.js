f64dc305a769fcd2bc4beb8c93f5f4abe2f25e1c
Ayan20april <112053665+Ayan20april@users.noreply.github.com>
2023-07-15 19:53:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
+/* for interval */
 var stopwatch;
+
+/* for store the starting time */
 var startTime;
+
+/* track whether the stopwatch currently running */
 var running = false;
+
+/* event listners are added. which trigger respective functions when clicked */ 
 var display = document.getElementById("display");
 var startButton = document.getElementById("start");
 var stopButton = document.getElementById("stop");
 var resetButton = document.getElementById("reset");
 
+/* this function starts the stopwatch by setting startTime */
 function start() {
   if (!running) {
     startTime = Date.now();
+    /* update the display every second */
     stopwatch = setInterval(updateDisplay, 1000);
     running = true;
   }
 }
 
+/* this function stops the stopwatch */
 function stop() {
   if (running) {
+    /* using this for clearing the interval */
     clearInterval(stopwatch);
     running = false;
   }
 }
 
+/* This fuction stops the stopwatch and resets the display */
 function reset() {
   stop();
   display.textContent = "00:00:00";
 }
 
+/* this fuction format the time into a hours, minutes and seconds */
 function updateDisplay() {
   var currentTime = Date.now();
   var elapsedTime = currentTime - startTime;
@@ -37,10 +50,11 @@ function updateDisplay() {
   hours = padTime(hours);
   minutes = padTime(minutes);
   seconds = padTime(seconds);
-
+  /* its update the display according this */
   display.textContent = hours + ":" + minutes + ":" + seconds;
 }
 
+/* This function ensures that the times values are always displays two digit */
 function padTime(time) {
   return time.toString().padStart(2, "0");
 }
